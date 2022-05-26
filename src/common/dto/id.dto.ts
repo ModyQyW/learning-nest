54baa364dc7e64d096a8543d7ae1d20a662c9f3c
ModyQyW <wurui-dev@foxmail.com>
2022-05-26 13:38:00 +0800

    feat: update common dto

--- src/common/dto/id.dto.ts
+++ src/common/dto/id.dto.ts
@@ -2,7 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsMongoId } from 'class-validator';
 
 export class IdDto {
-  @ApiProperty({ description: 'The id for the operation.', example: '62873690d3f83ca1cf641b45' })
+  @ApiProperty({
+    description: 'The id for the operation.<br />操作使用的 ID。',
+    example: '62873690d3f83ca1cf641b45',
+  })
   @IsMongoId()
   readonly id: string;
 }
--- src/common/dto/ids.dto.ts
+++ src/common/dto/ids.dto.ts
@@ -3,7 +3,10 @@ import { Transform } from 'class-transformer';
 import { IsMongoId } from 'class-validator';
 
 export class IdsDto {
-  @ApiProperty({ description: 'The ids for the operation.', example: ['62873690d3f83ca1cf641b45'] })
+  @ApiProperty({
+    description: 'The ids for the operation.<br />操作使用的多个 ID。',
+    example: ['62873690d3f83ca1cf641b45'],
+  })
   @Transform(({ value }) => (Array.isArray(value) ? value : value.split(',')))
   @IsMongoId({ each: true })
   readonly ids: string[];
