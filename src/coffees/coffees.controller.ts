801f38ad0b54274a2ee537615523fe05151661b1
ModyQyW <wurui-dev@foxmail.com>
2022-05-31 10:00:21 +0800

    feat: update swagger property

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get, Param, Patch, Body, Delete, Post, Query } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Body,
+  Delete,
+  Post,
+  Query,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { IdDto, IdsDto, PaginationDto } from '@/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto, UpdateCoffeeDto, UpdateBulkCoffeeDto } from './dto';
@@ -48,11 +59,20 @@ export class CoffeesController {
    */
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'Successfully create a coffee. <br />成功创建一个咖啡。',
+    type: Coffee,
+  })
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
     return this.coffeeService.create(createCoffeeDto);
   }
 
   @Post('/bulk')
+  @ApiCreatedResponse({
+    description: 'Successfully create bulk coffees. <br />成功创建多个咖啡。',
+    type: Coffee,
+    isArray: true,
+  })
   createBulk(@Body() createCoffeeDtos: CreateCoffeeDto[]) {
     return this.coffeeService.create(createCoffeeDtos);
   }
@@ -62,21 +82,41 @@ export class CoffeesController {
    */
 
   @Patch('/bulk')
+  @ApiOkResponse({
+    description: 'Successfully update bulk coffees. <br />成功更新多个咖啡。',
+    type: Coffee,
+    isArray: true,
+  })
   updateBulk(@Body() updateBulkCoffeeDtos: UpdateBulkCoffeeDto[]) {
     return this.coffeeService.updateBulk(updateBulkCoffeeDtos);
   }
 
   @Patch('/:id')
+  @ApiOkResponse({
+    description: 'Successfully update a coffee. <br />成功更新一个咖啡。',
+    type: Coffee,
+  })
   update(@Param() idDto: IdDto, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeeService.update(idDto, updateCoffeeDto);
   }
 
   @Post('/patch/bulk')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Successfully update bulk coffees. <br />成功更新多个咖啡。',
+    type: Coffee,
+    isArray: true,
+  })
   updateBulkWithPost(@Body() updateBulkCoffeeDtos: UpdateBulkCoffeeDto[]) {
     return this.updateBulk(updateBulkCoffeeDtos);
   }
 
   @Post('/:id/patch')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Successfully update a coffee. <br />成功更新一个咖啡。',
+    type: Coffee,
+  })
   updateWithPost(@Param() idDto: IdDto, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.update(idDto, updateCoffeeDto);
   }
@@ -86,21 +126,41 @@ export class CoffeesController {
    */
 
   @Delete('/bulk')
+  @ApiOkResponse({
+    description: 'Successfully delete bulk coffees. <br />成功删除多个咖啡。',
+    type: Coffee,
+    isArray: true,
+  })
   removeBulk(@Body() idsDto: IdsDto) {
     return this.coffeeService.removeBulk(idsDto);
   }
 
   @Delete('/:id')
+  @ApiOkResponse({
+    description: 'Successfully delete a coffee. <br />成功删除一个咖啡。',
+    type: Coffee,
+  })
   remove(@Param() idDto: IdDto) {
     return this.coffeeService.remove(idDto);
   }
 
   @Post('/delete/bulk')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Successfully delete bulk coffees. <br />成功删除多个咖啡。',
+    type: Coffee,
+    isArray: true,
+  })
   removeBulkWithPost(@Body() idsDto: IdsDto) {
     return this.removeBulk(idsDto);
   }
 
   @Post('/:id/delete')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    description: 'Successfully delete a coffee. <br />成功删除一个咖啡。',
+    type: Coffee,
+  })
   removeWithPost(@Param() idDto: IdDto) {
     return this.remove(idDto);
   }
