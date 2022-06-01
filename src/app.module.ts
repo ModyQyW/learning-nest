9698f250a58a9ae6c05554b659da97b707fa9a30
ModyQyW <wurui-dev@foxmail.com>
2022-06-01 10:32:33 +0800

    feat: use 0.0.0.0 instead of localhost

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { CoffeesModule } from './coffees';
 
 @Module({
   imports: [
     CoffeesModule,
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb://root:mongodb@localhost:27017,localhost:27018,localhost:27019/db?authSource=admin&replicaSet=replicaset',
+      'mongodb://root:mongodb@0.0.0.0:27017,0.0.0.0:27018,0.0.0.0:27019/db?authSource=admin&replicaSet=replicaset',
     ),
   ],
   controllers: [],
