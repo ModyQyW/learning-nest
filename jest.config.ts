95a7212bccff51359b0069b658df434966235543
ModyQyW <wurui-dev@foxmail.com>
2022-08-16 21:18:18 +0800

    chore: update config

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  collectCoverageFrom: ['**/*.(j|t)s'],
+  coverageDirectory: '../coverage',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
+
+export default config;
